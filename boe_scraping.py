c64af113e147c98c1bcb98299a192ada0c9fe5db
IvanGuGon1 <certified.courses@outlook.es>
2023-09-28 18:54:52 +0200

    Added all static types

--- boe_scraping.py
+++ boe_scraping.py
@@ -1,19 +1,18 @@
 import re
+from re import Pattern
 import shutil
 import PyPDF2
+from PyPDF2 import PdfFileReader,PageObject
 import tabula
 import sys
 import os
 import datetime
+from datetime import date
 import requests
+from requests import Response
 from unidecode import unidecode
-from typing import Tuple
-
-#matchings = re.findall(r"**\d{2}/\d{2}/\d{4}.*df", text)
-
-#print(matchings)
-
-
+from typing import Tuple,List
+import shutil
 
 def get_date() -> Tuple[str, str, str]:
     """
@@ -30,20 +29,20 @@ def get_date() -> Tuple[str, str, str]:
     return day, month, year
     
 
-def get_pdf_url_files(year,month,day):
+def get_pdf_url_files(year:str,month:str,day:str) -> List[str]:
 
-    boe_url_string = f"https://www.boe.es/boe/dias/{year}/{month}/{day}/"
-    page  = requests.get(boe_url_string)
+    boe_url_string:str = f"https://www.boe.es/boe/dias/{year}/{month}/{day}/"
+    page: Response  = requests.get(boe_url_string)
     
     if page.status_code == 200:
-        text = page.text 
-        pdf_url_files_list = []
+        text:str = page.text 
+        pdf_url_files_list:List[str] = []
         for line in text.splitlines():
             if f"/boe/dias/{year}/{month}/{day}/pdfs" in line:
-                patron = re.compile(r"BOE-[A-Z]-\d{4}-\d{5}.pdf")
+                patron:Pattern = re.compile(r"BOE-[A-Z]-\d{4}-\d{5}.pdf")
                 if len(patron.findall(line)) > 0:
-                    file_str_extract = patron.findall(line)[0]
-                    url = f"https://www.boe.es/boe/dias/{year}/{month}/{day}/pdfs/{file_str_extract}"
+                    file_str_extract:List[str] = patron.findall(line)[0]
+                    url:str = f"https://www.boe.es/boe/dias/{year}/{month}/{day}/pdfs/{file_str_extract}"
                     #print(url)
                     pdf_url_files_list.append(url)
         print(f"La url boe_url_string {boe_url_string} da ha sido encontrada: {page.status_code}")
@@ -53,17 +52,17 @@ def get_pdf_url_files(year,month,day):
                 
     return pdf_url_files_list
 
-def get_webpage_text(boe_url_string):
+def get_webpage_text(boe_url_string:str) -> str:
     
-    page  = requests.get(boe_url_string)
+    page:Response  = requests.get(boe_url_string)
     return page.text
 
 
-def download_pdf_files(url,path):
+def download_pdf_files(url:str,path:str) -> int:
     
-    destino = path + "/" + url.split("/")[-1]
+    destino:str = path + "/" + url.split("/")[-1]
 
-    response = requests.get(url)
+    response:Response = requests.get(url)
     if response.status_code == 200:
         with open(destino, 'wb') as file:
             file.write(response.content)
@@ -75,19 +74,17 @@ def download_pdf_files(url,path):
 
 
 
-def add_today_files_path():
-    import datetime
-    import os 
-    import shutil
+def add_today_files_path() -> str:
+
     # Obtener la fecha actual y la fecha de ayer
-    fecha_actual = datetime.date.today()
-    fecha_ayer = fecha_actual - datetime.timedelta(days=1)
+    fecha_actual:date = datetime.date.today()
+    fecha_ayer:date = fecha_actual - datetime.timedelta(days=1)
 
     # Formatear las fechas como cadenas (por ejemplo, "2023-09-26")
-    fecha_actual_formateada = fecha_actual.strftime("%Y-%m-%d")
-    fecha_ayer_formateada = fecha_ayer.strftime("%Y-%m-%d")
-    full_path_today = os.path.abspath(os.path.join("files",fecha_actual_formateada))
-    full_path_yesterday = os.path.abspath(os.path.join("files",fecha_ayer_formateada))
+    fecha_actual_formateada:str = fecha_actual.strftime("%Y-%m-%d")
+    fecha_ayer_formateada:str = fecha_ayer.strftime("%Y-%m-%d")
+    full_path_today:str = os.path.abspath(os.path.join("files",fecha_actual_formateada))
+    full_path_yesterday:str = os.path.abspath(os.path.join("files",fecha_ayer_formateada))
     # Crear el directorio con la fecha de hoy (si no existe)
     if not os.path.exists(full_path_today):
         try:
@@ -110,45 +107,24 @@ def add_today_files_path():
     return full_path_today
 
 
-def read_local_pdf_files(path):
-
-    # Directorio que deseas leer
-    directorio = path
-
+def read_local_pdf_files(path:str)-> List[str]:
     # Obtén una lista de todos los archivos en el directorio
-    archivos = os.listdir(directorio)
-    file_list = []
+    archivos:List[str] = os.listdir(path)
+    file_list:List[str] = []
     # Itera sobre la lista de archivos
     for archivo in archivos:
         # Verifica si el elemento en la lista es un archivo (no es un directorio)
-        if os.path.isfile(os.path.join(directorio, archivo)):
+        if os.path.isfile(os.path.join(path, archivo)):
             # Procesa el archivo aquí
             print("Nombre del archivo:", archivo)
-            file_list.append(os.path.join(directorio, archivo))
-    
+            file_list.append(os.path.join(path, archivo))
     return file_list
 
-def num_paginas(archivo_pdf):
-    """
-    Esta bien. Con Libreria PyPDf
-    """
+def find_strings_in_order(surname_1:str,surname_2:str,name:str,text:str)->bool:
     
-    with open(archivo_pdf, 'rb') as pdf_file:
-        # Crea un objeto PdfFileReader para leer el PDF
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-        # Obtiene el número total de páginas del PDF
-        total_de_paginas = pdf_reader.numPages
-        print(f"El numero de paginas es {total_de_paginas}")
-        return total_de_paginas
-
-
-    return contador_pags
-
-def find_strings_in_order(surname_1,surname_2,name,text):
-    
-    surname_1_char_position = text.find(surname_1)
-    surname_2_char_position = text.find(surname_2)
-    name_char_position = text.find(name)
+    surname_1_char_position:int = text.find(surname_1)
+    surname_2_char_position:int = text.find(surname_2)
+    name_char_position:int = text.find(name)
     
     if surname_1_char_position != -1 and surname_2_char_position != -1 and name_char_position != -1:
         print("Encontrados los apellidos y nombres")
@@ -162,68 +138,39 @@ def find_strings_in_order(surname_1,surname_2,name,text):
         print("Alguno de los datos no ha sido encontrado, por lo tanto no nos sirve")
         return False
 
-def find_person_in_boe(surname_1,surname_2,name,file_path):
-    """
-    Lo dejo por si necesitamos esta lógica mas adelante para comprobar cual encontramos primero. Usar findall mejor.
-    """
-    paginas = num_paginas(file_path)
-    print(f"El fichero boe_tipo_1.pdf Tiene este numero de paginas del documento es {paginas}")
-    finded_pages_counter = 0
-    for pagina in range(1,paginas+1):
-        print(f"Acceso a pagina: {pagina}")
-        try:
-            with open(file_path, "rb") as f:
-                finded_pages_counter = finded_pages_counter + 1
-                table = tabula.read_pdf(f, pages=pagina)
-                table_str = str(table)
-                find_strings = find_strings_in_order(surname_1,surname_2,name,table_str)
-                if find_strings:
-                    print("\n\n******\n\n")
-                    print(f"Notificacion de que la persona {surname_1} {surname_2} {name} tiene una entrada en el boe en la pagina {pagina}")
-                    finded_pages_counter = finded_pages_counter + 1
-                    return True
-                else:
-                    print(f" {surname_1} {surname_2} {name} NO ENCONTRADO en la pagina {pagina}")
-                    return False
-                    #print(f" {surname_1} {surname_2} {name} NO ENCONTRADO en la pagina {pagina}")
-                    #print(f" {surname_1} {surname_2} {name} NO ENCONTRADO en la pagina {pagina}")
-        except Exception as e:
-            print(e)
-            break
-        
 
-def transform_unidecode_string(string):
+def transform_unidecode_string(string:str)->str:
 
     return unidecode(string).upper()
 
 
-def search_strings_in_pdf(file_path,surname_1,surname_2,name):
+def search_strings_in_pdf(file_path:str,surname_1:str,surname_2:str,name:str)->bool:
     """
     Esta es la que escogemos
     """
     
     with open(file_path, 'rb') as pdf_file:
         # Crea un objeto PdfFileReader para leer el archivo PDF
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        pdf_reader: PdfFileReader  = PyPDF2.PdfFileReader(pdf_file)
 
         # Verifica si el archivo PDF se puede leer correctamente
         if pdf_reader.isEncrypted:
             pdf_reader.decrypt("")  # Si el PDF está protegido con contraseña, proporciónala aquí
 
         # Imprime el número de páginas en el PDF
-        num_paginas = pdf_reader.numPages
+        num_paginas:int = pdf_reader.numPages
         print(f"El PDF tiene con rut {file_path} {num_paginas} páginas.")
 
         # Lee el contenido de todas las páginas del PDF
         
-        surname_1 = transform_unidecode_string(surname_1)
-        surname_2 = transform_unidecode_string(surname_2)
-        name = transform_unidecode_string(name)
+        surname_1:str = transform_unidecode_string(surname_1)
+        surname_2:str = transform_unidecode_string(surname_2)
+        name:str = transform_unidecode_string(name)
         
-        finded = False
+        finded:bool = False
         for pagina_numero in range(num_paginas):
-            pagina = pdf_reader.getPage(pagina_numero)
-            texto = transform_unidecode_string(pagina.extractText())
+            pagina:PageObject = pdf_reader.getPage(pagina_numero)
+            texto:str = transform_unidecode_string(pagina.extractText())
             """
             patron = rf"\b{surname_1}\b.*\b{surname_2}\b.*\b{name}\b"
             coincidencias = re.search(patron, texto)
@@ -252,9 +199,9 @@ def search_strings_in_pdf(file_path,surname_1,surname_2,name):
 
             if surname_1 in texto and surname_2 in texto and name in texto:
                 # Logica repetida, la dejo por si decido implementar el orden en el que encuentro nombre y apellidos. Find me devuelve la posicion del caracter
-                surname_1_char_position = texto.find(surname_1)
-                surname_2_char_position = texto.find(surname_2)
-                name_char_position = texto.find(name)
+                surname_1_char_position:int = texto.find(surname_1)
+                surname_2_char_position:int = texto.find(surname_2)
+                name_char_position:int = texto.find(name)
                 
                 if surname_1_char_position != -1 and surname_2_char_position != -1 and name_char_position != -1:
                     if surname_1_char_position < surname_2_char_position < name_char_position:
@@ -276,21 +223,21 @@ if __name__ == "__main__":
     surname_2 = transform_unidecode_string(sys.argv[2])
     name = transform_unidecode_string(sys.argv[3])
     """
-    surname_1 = "MARTIN"
-    surname_2 = "GUERRERO"
-    name = "IVAN"
+    surname_1:str = "MARTIN"
+    surname_2:str = "GUERRERO"
+    name:str = "IVAN"
     """
     Prueba de funcion get_date
     """
     print("\n\n   Prueba de funcion get_date \n\n")
     print(get_date())
-    day,month,year = get_date()
+    day, month,year = get_date()
     
     """
     Prueba de funcion get_pdf_url_files
     """
     print("\n\n   Prueba de funcion get_pdf_url_files \n\n")
-    pdf_url_files_list = get_pdf_url_files(year,month,day) 
+    pdf_url_files_list:List[str] = get_pdf_url_files(year,month,day) 
     for pdf_url_file in pdf_url_files_list:
         print(pdf_url_file)
     
@@ -299,7 +246,7 @@ if __name__ == "__main__":
     """
     print("\n\n   Prueba de funcion add_path \n\n")
 
-    today_files_path = add_today_files_path()
+    today_files_path:str = add_today_files_path()
     print(today_files_path)
     
     """
@@ -321,10 +268,10 @@ if __name__ == "__main__":
     print("\n\n   Prueba de funcion read_pdf_files \n\n")
     print("\n\n\n*10")
     
-    file_list = read_local_pdf_files(today_files_path)
+    file_list:List[str] = read_local_pdf_files(today_files_path)
     for file in file_list:
         print(file)
-        person_found = search_strings_in_pdf(file,surname_1,surname_2,name)
+        person_found:bool = search_strings_in_pdf(file,surname_1,surname_2,name)
         if person_found:
             # Aqui va la logica de la noticificación, enviar a email, telegram etc.....
             print("****************")
